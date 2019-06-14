0714d6313a0290c63c9090e11a99741a0e11792e
silviolimeira <sicemal@gmail.com>
2019-06-14 19:59:58 -0300

    Test component and service

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { HelloService } from "../services/hello/hello.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  constructor(private helloService: HelloService) {}
 
-  constructor() {}
-
+  ngOnInit(): void {
+    try {
+      this.helloService.hello();
+    } catch (err) {
+      console.log(err);
+    }
+  }
 }
