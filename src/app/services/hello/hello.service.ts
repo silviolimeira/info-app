7f249b2f59d644444127fb751115b6609d695003
silviolimeira <sicemal@gmail.com>
2019-06-14 19:59:28 -0300

    update hello.service.ts

--- src/app/services/hello/hello.service.ts
+++ src/app/services/hello/hello.service.ts
@@ -1,9 +1,14 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class HelloService {
+  hello() {
+    throw new Error("Method not implemented.");
+  }
 
-  constructor() { }
+  constructor() {
+    console.log("HelloService ready!");
+  }
 }
