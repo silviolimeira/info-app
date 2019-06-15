bfb3adc94cd7b890c8321e982f284e6713867bf8
silviolimeira <sicemal@gmail.com>
2019-06-14 21:47:33 -0300

    binding a path and using Angular pipe async in the template to get the value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,35 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+// import { Platform } from "@ionic/angular";
+// import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+// import { StatusBar } from "@ionic-native/status-bar/ngx";
+
+import { Observable } from "rxjs";
+import { AngularFireDatabase } from "@angular/fire/database";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html'
+  selector: "app-root",
+  templateUrl: "app.component.html"
 })
 export class AppComponent {
+  infos: Observable<any[]>;
+
   constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    // private platform: Platform,
+    // private splashScreen: SplashScreen,
+    // private statusBar: StatusBar
+    private db: AngularFireDatabase
   ) {
+    this.infos = this.db.list("/infos/-LhAkCZR7_6XwHGQSVmZ").valueChanges();
+
     this.initializeApp();
   }
 
   initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
+    // this.platform.ready().then(() => {
+    //   this.statusBar.styleDefault();
+    //   this.splashScreen.hide();
+    // });
+    // this.infos.subscribe()
   }
 }
